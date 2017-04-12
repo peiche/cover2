68eb7f5e0558fe86c700a209b2e90059d52e4b8d
Paul <peiche@gmail.com>
2017-04-12 13:10:34 -0500

    Added separate navbar color.

--- inc/customizer.php
+++ inc/customizer.php
@@ -25,6 +25,12 @@ function cover2_customize_register( $wp_customize ) {
 
 	// Remove the core header textcolor control.
 	$wp_customize->remove_control( 'header_textcolor' );
+	
+	// Add setting for header color.
+	$wp_customize->add_setting( 'header_color', array(
+		'default'           => '#4020df',
+		'sanitize_callback' => 'sanitize_hex_color',
+	) );
 
 	// Add setting for accent color.
 	$wp_customize->add_setting( 'accent_color', array(
@@ -39,25 +45,34 @@ function cover2_customize_register( $wp_customize ) {
 		'transport'         => 'postMessage',
 		'sanitize_callback' => 'cover2_sanitize_overlay_colorscheme',
 	) );
+	
+	// Add control for header color.
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_color', array(
+		'label'		    	=> __( 'Navbar Color', 'cover2' ),
+		'description'		=> __( 'Applied to the top bar on the blog homepage and when scrolling down the page.', 'cover2' ),
+		'section'	    	=> 'colors',
+		'priority'			=> 5,
+	) ) );
 
 	// Add control for accent color.
 	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'accent_color', array(
-		'label'    => __( 'Accent Color', 'cover2' ),
-		'mode'     => 'hue',
-		'section'  => 'colors',
-		'priority' => 5,
+		'label'   	 		=> __( 'Accent Color', 'cover2' ),
+		'description'		=> __( 'Applied to the page header (when there is no featured image) and links.', 'cover2' ),
+		'mode'     			=> 'hue',
+		'section'  			=> 'colors',
+		'priority' 			=> 6,
 	) ) );
 
 	// Add control for overlay color scheme.
 	$wp_customize->add_control( 'overlay_colorscheme', array(
-		'type'    => 'radio',
-		'label'    => __( 'Overlay Color Scheme', 'cover2' ),
-		'choices'  => array(
-			'light'  => __( 'Light', 'cover2' ),
-			'dark'   => __( 'Dark', 'cover2' ),
+		'type'   			=> 'radio',
+		'label'    			=> __( 'Overlay Color Scheme', 'cover2' ),
+		'choices'  			=> array(
+			'light'  			=> __( 'Light', 'cover2' ),
+			'dark'   			=> __( 'Dark', 'cover2' ),
 		),
-		'section'  => 'colors',
-		'priority' => 6,
+		'section'  			=> 'colors',
+		'priority' 			=> 7,
 	) );
 
 }
