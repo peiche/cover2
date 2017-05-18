b0eda9015a2559b7bf2ec1baaea9f898b068e3ac
Paul <peiche@gmail.com>
2017-05-17 21:21:09 -0500

    Removed unused heading

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			
 		<?php
 		if ( is_home() && ! is_paged() ) {
 		  // Include the featured content template.
@@ -26,13 +26,6 @@ get_header(); ?>
 
 		<?php
 		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php
-			endif;
 
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
