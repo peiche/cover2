6cf67dcfa43d22c0b4efeb5528954170089d3717
Paul <peiche@gmail.com>
2017-07-05 23:03:52 -0500

    Replaced pseudo-element overlay with real element

--- inc/aesop.php
+++ inc/aesop.php
@@ -12,3 +12,9 @@ function cover2_quote_size_unit() {
   return 'rem';
 }
 add_filter( 'aesop_quote_size_unit', 'cover2_quote_size_unit' );
+
+function cover2_content_overlay() {
+    echo '<div class="aesop-content-darken"></div>';
+    return;
+}
+add_action('aesop_cbox_content_inside_top', 'cover2_content_overlay');
