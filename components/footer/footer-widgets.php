86551b39e0a64460c112316b0d57bcfb5e1a3dd5
Paul <peiche@gmail.com>
2018-01-30 13:15:10 -0600

    Fixed footer widget alignment.

--- components/footer/footer-widgets.php
+++ components/footer/footer-widgets.php
@@ -9,7 +9,7 @@
 
 <?php if ( is_active_sidebar( 'sidebar-footer-1' ) || is_active_sidebar( 'sidebar-footer-2' ) || is_active_sidebar( 'sidebar-footer-3' ) ) : ?>
     <div class="footer-widgets">
-        <div class="container">
+        <div class="container large">
             
             <div class="col-3">
                 <?php if ( is_active_sidebar( 'sidebar-footer-1' ) ) : ?>
