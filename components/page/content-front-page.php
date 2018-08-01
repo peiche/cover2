701f0f203d294a92c1f394e684a4c6a70bec6881
Paul <peiche@gmail.com>
2018-08-01 15:55:38 -0500

    Moved header into separate file.

--- components/page/content-front-page.php
+++ components/page/content-front-page.php
@@ -11,16 +11,24 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="page-header">
+	<header class="page-header<?php if ( '' != cover2_get_featured_image( get_the_ID() ) || '' != get_header_image() ) : echo ' full-height'; endif; ?>">
 		<div class="page-header__image"></div>
 		
 		<div class="page-header__content text-align-center">
-			<h1 class="site-title page-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title page-title"><?php bloginfo( 'name' ); ?></h1>
 			<hr />
 			<div class="site-description entry-excerpt">
 				<?php bloginfo( 'description' ); ?>
 			</div>
 		</div>
+		
+		<?php if ( '' != cover2_get_featured_image( get_the_ID() ) || '' != get_header_image() ) : ?>
+			<div class="page-header__scroll-container">
+				<a class="page-header__scroll-link" href="#post-<?php the_ID(); ?>-content">
+					<?php echo cover2_get_svg( array( 'icon' => 'chevron-down' ) ); ?>
+				</a>
+			</div>
+		<?php endif; ?>
 	</header>
 
 	<div id="post-<?php the_ID(); ?>-content" class="entry-content aesop-entry-content">
