c4b3138d067180dc6f8a3c8cee357bc370976444
Paul <peiche@gmail.com>
2018-08-28 07:52:29 -0500

    Adjusted markup for Flexbox.

--- components/header/header-branding.php
+++ components/header/header-branding.php
@@ -8,23 +8,25 @@
 ?>
 
 <div class="site-branding">
-	<?php
-	if ( has_custom_logo() ) :
-		the_custom_logo();
-	endif;
-	?>
+	<div class="site-meta">
+		<?php
+		if ( has_custom_logo() ) :
+			the_custom_logo();
+		endif;
+		?>
+		
+		<?php if ( ! has_custom_logo() || ( has_custom_logo() && ( ! is_front_page() || ! is_page() ) ) ) : ?>
+			<h1 class="site-title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</h1>
+		<?php endif; ?>
+		
+		<?php if ( has_custom_logo() && ( ! is_front_page() || ! is_page() ) ) : ?>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		<?php endif; ?>
+	</div>
 	
-	<?php if ( ! has_custom_logo() || ( has_custom_logo() && ( ! is_front_page() || ! is_page() ) ) ) : ?>
-		<h1 class="site-title">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-		</h1>
-	<?php endif; ?>
-	
-	<?php if ( has_custom_logo() && ( ! is_front_page() || ! is_page() ) ) : ?>
-		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-	<?php endif; ?>
-
 	<?php if ( has_nav_menu( 'mini' ) ) : ?>
 		<?php wp_nav_menu( array( 'theme_location' => 'mini', 'container_class' => 'mini-menu-container' ) ); ?>
 	<?php endif; ?>
-</div><!-- .site-branding -->
+</div>
